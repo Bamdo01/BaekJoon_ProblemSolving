d516e995929ce236011945a8b03d717a47bad7d5
범수 <80352847+Bamdo01@users.noreply.github.com>
2024-08-22 14:51:48 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+
+	int s;
+	cin >> s;
+
+	for (int i = 0; i < s; i++)
+	{
+		int a, b, c;
+		cin >> a >> b;
+		c = a + b;
+
+		cout << c << "\n";
+	}
+}

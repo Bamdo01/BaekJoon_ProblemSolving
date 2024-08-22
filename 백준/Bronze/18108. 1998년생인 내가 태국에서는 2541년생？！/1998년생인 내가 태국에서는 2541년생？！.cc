dc4350d5d535e7f2e150a5220ed21e5038c79313
범수 <80352847+Bamdo01@users.noreply.github.com>
2024-08-22 14:45:10 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+//우리나라는 서기 연도를 사용하고 있다.불기 연도가 주어질 때 이를 서기 연도로 바꿔 주는 프로그램을 작성하시오.
+int main() 
+{
+	int a;
+	int b;
+
+	cin >> a;
+
+	b = a - 543;
+
+	cout << b;
+
+	
+}

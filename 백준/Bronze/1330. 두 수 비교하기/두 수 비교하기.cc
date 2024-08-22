2c2848c6b4a01876f815b3681362cbf702a4df72
범수 <80352847+Bamdo01@users.noreply.github.com>
2024-08-22 14:47:32 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+//두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+using namespace std;
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+
+	if (a > b)
+	{
+		cout << '>';
+
+	}
+	else if (a < b)
+	{
+		cout << '<';
+	}
+	else
+	{
+		cout << "==";
+	}
+}

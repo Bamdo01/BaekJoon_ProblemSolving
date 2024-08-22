a60b74eb16e79d3f12a3d9a237517f9aa67d9814
범수 <80352847+Bamdo01@users.noreply.github.com>
2024-08-22 14:46:48 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+//꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+int main(){
+	long a, b, c, z;
+
+
+	cin >> a >> b >> c;
+
+	z = a + b + c;
+
+	cout << z << "\n";
+
+
+	
+}
